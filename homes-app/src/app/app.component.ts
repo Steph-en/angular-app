972c4077106aa04ca1b1448bcae407900ce6b45d
Stephen Appah <appahstephen9@gmail.com>
2024-02-18 15:44:21 +0000

    Merge fix

--- homes-app/src/app/app.component.ts
+++ homes-app/src/app/app.component.ts
@@ -1,27 +1,11 @@
-import { Component } from "@angular/core";
-import { HomeComponent } from "./home/home.component";
+import { Component } from '@angular/core';
 
 @Component({
-    standalone: true,
-    selector: "app-root",
-    template: `
-    <main>
-      <header class="brand-name">
-        <img
-          src="/assets/logo.svg"
-          alt="logo"
-          class="brand-logo"
-          aria-hidden="true"
-        />
-      </header>
-      <section class="content">
-        <app-home></app-home>
-      </section>
-    </main>
-  `,
-    styleUrls: ["./app.component.css"],
-    imports: [HomeComponent]
+  standalone: true,
+  selector: 'app-root',
+  template: `<h1>Hello Universe!</h1>`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = "homes";
+  title = 'homes';
 }
