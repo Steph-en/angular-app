0bc9b2accadde3f00f693a00c8a111bfb4ced0b4
Stephen Appah <appahstephen9@gmail.com>
2024-03-11 10:07:44 +0000

    Feat: Created Details component

--- homes-app/src/app/housing-location/housing-location.component.ts
+++ homes-app/src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HousingLocation } from "../housing-location";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: "app-housing-location",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
     <section class="listing">
       <img
@@ -17,8 +18,9 @@ import { HousingLocation } from "../housing-location";
         {{ housingLocation.name }}
       </h2>
       <p class="listing-location">
-        {{ housingLocation.city}}, {{ housingLocation.state }}
+        {{ housingLocation.city }}, {{ housingLocation.state }}
       </p>
+      <a [routerLink]="['details', housingLocation.id]">Learn more</a>
     </section>
   `,
   styleUrls: ["./housing-location.component.css"],
